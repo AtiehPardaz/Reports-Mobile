bff132bcffdb2a9f738b5133ed468cac3d9b6c1a
behzad zadpour <bzadpoor@gmail.com>
2015-03-04 19:16:23 +0330

    Task 410   :    طراحی صفحه ورود

--- Reportsmobile/gen/com/Atieh/reportsmobile/R.java
+++ Reportsmobile/gen/com/Atieh/reportsmobile/R.java
@@ -1632,9 +1632,16 @@ containing a value of this type.
         public static final int abc_textfield_search_activated_mtrl_alpha=0x7f020030;
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020031;
         public static final int abc_textfield_search_material=0x7f020032;
-        public static final int ic_launcher=0x7f020033;
+        public static final int background=0x7f020033;
+        public static final int blackt=0x7f020034;
+        public static final int ic_launcher=0x7f020035;
+        public static final int img_btn_vorod=0x7f020036;
+        public static final int loadinglogo=0x7f020037;
+        public static final int logo=0x7f020038;
     }
     public static final class id {
+        public static final int ImageView01=0x7f090045;
+        public static final int LinearLayout1=0x7f09003f;
         public static final int action_bar=0x7f090031;
         public static final int action_bar_activity_content=0x7f090003;
         public static final int action_bar_container=0x7f090030;
@@ -1648,7 +1655,7 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f09003f;
+        public static final int action_settings=0x7f090047;
         public static final int activity_chooser_view_content=0x7f090021;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
@@ -1661,6 +1668,8 @@ containing a value of this type.
         public static final int dropdown=0x7f09001a;
         public static final int edit_query=0x7f090033;
         public static final int end=0x7f09001c;
+        public static final int et_password=0x7f090042;
+        public static final int et_username=0x7f090041;
         public static final int expand_activities_button=0x7f090022;
         public static final int expanded_menu=0x7f090028;
         public static final int home=0x7f090000;
@@ -1668,12 +1677,16 @@ containing a value of this type.
         public static final int icon=0x7f090026;
         public static final int ifRoom=0x7f090016;
         public static final int image=0x7f090023;
+        public static final int img_btn_login=0x7f090043;
+        public static final int linear1=0x7f090040;
+        public static final int linearpb=0x7f090044;
         public static final int listMode=0x7f09000a;
         public static final int list_item=0x7f090025;
         public static final int middle=0x7f09001d;
         public static final int never=0x7f090017;
         public static final int none=0x7f09000f;
         public static final int normal=0x7f09000b;
+        public static final int progressBar1=0x7f090046;
         public static final int progress_circular=0x7f090007;
         public static final int progress_horizontal=0x7f090008;
         public static final int radio=0x7f09002b;
